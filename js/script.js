81ee64a65c5952ee184e8f1c3359511042d4aacb
LizaGolubeva <sp6lisa@yandex.ru>
2021-08-30 22:53:50 +0300

    Поменьше комментариев

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // TABS
-    const tabs = document.querySelectorAll('.tabheader__item'),//получили все div стили питания
-          tabsContent = document.querySelectorAll('.tabcontent') ,//получили контейнеры-слайды
-          tabsParent = document.querySelector('.tabheader__items');//получили родителя tabs для делегирования событий
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent') ,
+          tabsParent = document.querySelector('.tabheader__items');
 
 
     function hideTabContent(){//скрыла весь контент
@@ -24,14 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
         tabs[i].classList.add('tabheader__item_active');
     }
 
-
     hideTabContent();
     showTabContent();
-    //если функция вызовется без аргумента, то в функции можно написать аргумент по умолчанию (i = 0)
 
 
     tabsParent.addEventListener('click', (event) => {
-        const target = event.target;//сокращение просто
+        const target = event.target;//сокращение 
 
         if (target && target.classList.contains('tabheader__item')){
             console.log(target);
@@ -49,26 +47,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+    // SLIDER
+    const currSlide = document.getElementById('current');
+    const totalSlide = document.getElementById('total');
+    const slides = document.querySelectorAll('.offer__slide');
+    const leftArrow = document.querySelector('.offer__slider-prev');
+    const rightArrow = document.querySelector('.offer__slider-next');
+    let id = 0;//можно было и id = 1
+
+
+    if (slides.length < 10){
+        totalSlide.innerHTML = `0${slides.length}`;
+    } else {totalSlide.innerHTML = slides.length;}
+
+
+    function showSlide (id) {
+        slides.forEach(item =>{
+            item.classList.add('hide');
+            item.classList.remove('show', 'animation');
+        });//скрыли все
+
+        //показали нужный
+        slides[id].classList.add('show', 'animation');
+        slides[id].classList.remove('hide');
+
+        //вывели номер слайда
+        if (id + 1 < 10) {
+            currSlide.innerHTML = `0${id + 1}`;
+        } else {currSlide.innerHTML = id + 1;}
+    }
+
+    showSlide(id);
+
+    rightArrow.addEventListener('click', (event) => {
+        id++;
+        if (id === slides.length) {
+            id = 0;
+        }
+        showSlide(id);
+    });
+
+    leftArrow.addEventListener('click', (event) => {
+        id--;
+        if (id === -1) {
+            id = slides.length - 1;
+        }
+        showSlide(id);
+    });
+
+
+
+
+
+
+
+
     // TIMER
 
-    const deadline = '2021-07-24';
+    const deadline = '2021-08-24';
 
     //функция, определяющая разницу между дедлайном и текущем временем
     function getTimeRemaining(endtime){
 
         const t = Date.parse(endtime) - Date.parse(new Date());
         //количество миллисекунд в конечном времени, до которого нужно досчитать
-        //parse возвращает количество миллисекунд, прошедших с 1 января 1970 года 00:00:00 по UTC.
         
         //теперь надо эти миллисекунды нужно превратить в часы, минуты и тп
-        const days = Math.floor(t / 86400000);//округление до ближайшего меньшего целого.
-        //число в знаменателе - 1000 * 60 * 60 * 24
+        const days = Math.floor(t / 86400000);
 
         const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        //t делим на количество часов. но часов там может быть и 150 и тп.
-        //% делит на 24 и возвращает остаток от деления (то, что не хватает до полный суток)
+        //% делит на 24 и возвращает остаток от деления (то, что не хватает до полных суток)
         
-        const minutes = Math.floor((t / (1000 * 60)) % 60);//хвостик от минут в часах
+        const minutes = Math.floor((t / (1000 * 60)) % 60);
         const seconds = Math.floor((t / 1000) % 60);
 
 
@@ -85,7 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //функция добавления в день и часы 0 перед цифрой, если число меньше 10 (частое требование)
     function getZero(num){
         if (num >=0 && num<10){
-            return `0${num}`;//только уже строчка, но не важно
+            return `0${num}`;
         } else {
             return num;
         }
@@ -97,7 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
               hours = timer.querySelector('#hours'),
               minutes = timer.querySelector('#minutes'),
               seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000);//каждую секунду
+              timeInterval = setInterval(updateClock, 1000);
 
         updateClock();//вручную вызываем функцию, тк из-за setInterval она сработает только через секунду
 
@@ -126,20 +178,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // MODAL WINDOW
 
     //для того, чтобы обратиться к элементу-триггеру модального окна, можно использовать не класс и тег, а data-атрибуты,
-    //тк как теги так и классы могут меняться
 
     const modalOpen = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('.modal');
-    
-    //для одной кнопки
-    // modalOpen.addEventListener('click', () => {
-    //     modal.classList.add('show');
-    //     modal.classList.remove('hide');
-    //     document.body.style.overflow = 'hidden';
-    // });
 
-    
-    //открываем модальное окно
     function openModal () {
         modal.classList.add('show');
         modal.classList.remove('hide');
@@ -155,14 +197,6 @@ document.addEventListener('DOMContentLoaded', () => {
         btn.addEventListener('click', openModal);
     });
 
-
-    // modalClose.addEventListener('click', () => {
-    //     modal.classList.remove('show');
-    //     modal.classList.add('hide');
-    //     document.body.style.overflow = '';
-    // });
-
-
     //переделаем в функцию, тк закрытие встречается несколько раз
     function closeModal() {
         modal.classList.remove('show');
@@ -170,21 +204,13 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    // modalClose.addEventListener('click', closeModal);//здесь не нужно объявление
-
 
     //закрытие по темному полю в пределах модального окна
     modal.addEventListener('click', (e) =>{
-        //обязательно е в скобках
-        // console.log(e.target);
-
         //если нажимаем на подложку или на любой крестик (делегирование события крестика)
         if (e.target === modal || e.target.getAttribute('data-close') == ''){
             //проверяет нажатие по тегу с классом modal, а если нажать модальное окно, то выведет
             // уже другой тег с классом. нажимаю на input - выведет input, а темное поле - только диф с классом modal
-            // modal.classList.remove('show');
-            // modal.classList.add('hide');
-            // document.body.style.overflow = '';
             closeModal();
         }
     });
@@ -201,13 +227,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    //гуглить event.code
 
-
-
-
-    //модальное окно всплывает через время, при этом если пользователь сам нажал
-    //на модальное окно, то оно не всплывает
 
     // setTimeout позволяет вызвать функцию ОДИН раз через определённый интервал времени.
     // setInterval позволяет вызывать функцию РЕГУЛЯРНО, повторяя вызов через определённый интервал времени.
@@ -217,11 +237,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //открывается модальное окно, когда долистали страницу до конца
     //событие scroll на глобальном объекте window
-
-    //pageYOfset - сколько пикселей отлисталось сверху
-    //scrollheight - вся высота контента
-    //сколько отлисталось + высота окна клиента = вся высота контента => пользователь
-    //долистал до конца.
 
     const doc = document.documentElement;
 
@@ -235,8 +250,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
-    //}, {once: true}); //есть такое действие, но в данном случае не сработает
-    //тк сработает 1 раз, когда хоть немного пролистнем
 
 
 
@@ -276,10 +289,6 @@ document.addEventListener('DOMContentLoaded', () => {
             //создаем карточку
             const element = document.createElement('div');
 
-            // this.classes = classes || 'menu__item'; 
-            //1) classes пустой - будет пустой массив, что все равно true
-            //2) строку menu__item мы прогоним через forEach = ошибка
-
             //через условие проверим пустоту массива и тогда присвоим значение по умолчанию
             //массив проверять через length
             if(this.classes.length == 0){
@@ -305,25 +314,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    //могли бы сделать
-    // const divCard = new MenuCards();//+ аргументы
-    // divCard.writeHtml();
-
-    //но есть способ проще. можно не создавать переменную и её вызывать. можно сразу создать объект, сделать операцию
-    // и он сразу удалится. мы нигде его больше не сможем вызвать
-
-    // new MenuCards(
-    //     "img/tabs/vegy.jpg", //лучше передавать в кавычках, чем ставить их в интерполяции
-    //     "vegy",
-    //     'Меню "Фитнес"',
-    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    //     7,
-    //     '.menu .container'
-    //     // 'menu__item',//класс без . перед, тк добавляется в classlist
-    //     // 'big',//еще какой то класс
-    //     // 'red'
-    // ).writeHtml();
-
 
     //делает get запрос для формирования карточек
     const getResourse = async (url) => {
@@ -334,8 +324,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (!res.ok) {
             //объект ошибки
-
-            //"выкидываем" новую ошибку в консоли
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         
@@ -355,31 +343,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 new MenuCards(img, altimg, title, descr, price, '.menu .container').writeHtml();
             });
         });
-
-    //есть другой способ добавления карточек без использования классов
-    //подойдет, если не нужна шаблонизация, нужно создать что-то пару раз
-
-    // getResourse('http://localhost:3000/menu')//придет массив с объектами карточек
-    //     .then(data => createCard(data));
-
-    // function createCard(data) {
-    //     data.forEach(({img, altimg, title, descr, price}) => {
-    //         const element = document.createElement('div');
-    //         element.classList.add('menu__item');
-
-    //         element.innerHTML = `
-    //             <img src=${img} alt=${altimg}>
-    //             <h3 class="menu__item-subtitle">${title}</h3>
-    //             <div class="menu__item-descr">${descr}</div>
-    //             <div class="menu__item-divider"></div>
-    //             <div class="menu__item-price">
-    //                 <div class="menu__item-cost">Цена:</div>
-    //                 <div class="menu__item-total"><span>${price}</span> руб/день</div>
-    //             </div> 
-    //         `;
-    //         document.querySelector('.menu .container').append(element);
-    //     });
-    // }
 
     
 
@@ -415,7 +378,6 @@ document.addEventListener('DOMContentLoaded', () => {
         //делаем запрос и можем сразу обработать
 
         //это асинхронный код, поэтому fetch может идти долго, поэтому в переменную ничего не помещается
-        //const res = 4+4; - сначала выполнится действие справа, потом слева
         //механизм, превращающий асинхронный код в синхронный 
         //async (перед функцией, типо в функции есть синхронный код), await (используются в паре)
 
@@ -430,15 +392,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         
         //перевод тоже может долго идти
-        return await res.json();//это промис
+        return await res.json();
     };
 
 
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();//убираем перезагрузку сервера при отправке формы
-            //console.log(e);//событие SubmitEvent
-
             const statusMessage = document.createElement('img');
             statusMessage.src = message.loading;
             statusMessage.style.cssText = `
@@ -453,24 +413,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             //formdata - спец объект, который позволяет с формы формировать
             //данные в формате ключ-значение
-            
-            //для JSON нужен заголовок
-            // request.setRequestHeader('Content-type', 'application/json');
 
             const formData = new FormData(form);//передаетя форма из арг функции
             //!в input необходимо всегда указывать атрибут name, иначе formdata не сможем работать
-            
-            // console.log(formData);//какую то хрень выведет
 
-            // перевод formdata в json
-            // const object = {};
-            // formData.forEach(function(value, key){
-            //     object[key] = value;
-            // });
-
-            //ещё один метод перевода formdata в формат json
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
-            //нужно для перевода в json передать объект, entries делает массив, from - в объект
 
             // const obj2 = {a: 23, B: 50};
             // console.log(Object.entries(obj2));
@@ -492,20 +439,6 @@ document.addEventListener('DOMContentLoaded', () => {
             .finally(() => {
                 form.reset();
             });
-
-            // fetch('server.php', {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-type': 'application/json'
-            //     },
-            //     body: JSON.stringify(object) 
-            // })
-
-            //при отработке блока catch в модальном окне ничего не поменялось
-            //промис, запущенный fetch не перейдет в состояние rejected из-за ответа http 400-500 ошибки
-            //там только статус поменяется 
-            //для fetch важно, что он смог сделать запрос, поэтому он не обратит внимания на ошибки http
-            //вот если мы без инета запрос сделаем, тогда сработает catch
         }); 
     }
 
@@ -543,59 +476,4 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/menu')
     .then(data => data.json())
     .then(res => console.log(res));
-
-
-
-
-    // FETCH API
-    //встроена в браузер, чтобы общаться с сервером
-
-    //в скобках - запрос на url, если нет других параметров - get запрос
-    //возвращается промис
-    // fetch();
-    // fetch('https://jsonplaceholder.typicode.com/todos/1')
-
-    //много свойств, важные - метод и бади
-    //отправляем пост запрос
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: "POST",
-    //     body: JSON.stringify({name: 'Alex'}),//отправляем в формате json
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-    // .then(response => response.json())
-    // //метод response.json() превратит данные из json в объект на js
-    // //но возвращается промис
-    // //response.text()
-    // .then(json => console.log(json)); 
-    // //вывел {name: "Alex", id: 101}, где id - номер записи, на сайте указано, что там было их 100
-    // //значит код правильно отработал и мы добавили новую запись
-
-
-
-    //npm - часть кода, которая лежит на отдельном сервере, чтоб мы могли ими
-    //пользоваться в своем проекте (пакетный менеджер) (gulp)
-
-    //сборщики проектов, компиляторы css  тп. системе нужно сказать, что у нас npm-проект,
-    //то есть имеются npm пакеты.
-
-    //перед тем, как установить npm в проект нужно создать npm проект
-    //открываем терминал: в папке проекта => инициализация npm проекта - npm init
-    //1) название проекта (enter), 2) версия, 3) описание
-
-    //установка npm пакета: 
-    //1) json сервер  - npm install json-server (если глобально, то -g добавляем) 
-    // как пакет влияет на проект: при разработке --save-dev (тестовая фича), 
-    // для работы проекта внутри --save
-
-    //добавила в проект файл gitignore, тк node_modules может весить оч много, при том что
-    //проект весит мало - на репозиторий сохранять это такое себе
-
-    //разработали проект, запушили на github, другой скачал, видит, что node_modules у него нет
-    //просто прописывает в терминале npm i: все зависимости есть в файле package.json (от него и установится modules)
-
-    //json-server - симуляция backenda
-    
-    // ЗАПУСК JSON.SERVER (npx json-server db.json)
 });
